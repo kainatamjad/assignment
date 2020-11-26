ec65b32eb066c142cf43dd71e73a129295a3e599
hayer <kainatammjad555@gmail.com>
2020-11-26 12:12:42 -0800

    1

---
+++ java/assign.java
@@ -0,0 +1,14 @@
+
+ public class assign
+ {
+   public static void main(String[] args)
+   {
+     int numDimes = 7;
+     int numQuarters = 3;
+     int numNickels = 8;
+
+     int total = numDimes * 10 + numQuarters + 25;
+
+     System.out.println("Total = " + total);
+   }
+ }
